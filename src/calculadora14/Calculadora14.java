7522c726700d5625b781a0eb1ceaa5771be0d46f
Oier Morate <oiertopro@gmail.com>
2026-02-05 12:55:17 +0100

    Corregido el metodo dividir (dividir entre 0)

--- src/calculadora14/Calculadora14.java
+++ src/calculadora14/Calculadora14.java
@@ -19,6 +19,11 @@ public class Calculadora14 {
 	
 	public double dividir(int a, int b) 
 	{
+		 if (b == 0) 
+		 {
+			 throw new IllegalArgumentException("No se puede dividir entre cero");
+		 }
 		 return (double) a / b;
 	}
+
 }
