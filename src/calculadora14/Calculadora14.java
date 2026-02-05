a791dce89ef6c8f9c72e33e3f34d2561ecc389da
Oier Morate <oiertopro@gmail.com>
2026-02-05 12:51:50 +0100

    Metodos multiplicar y dividir

--- src/calculadora14/Calculadora14.java
+++ src/calculadora14/Calculadora14.java
@@ -11,4 +11,14 @@ public class Calculadora14 {
 	{
 		 return a - b;
 	}
+	
+	public int multiplicar(int a, int b) 
+	{
+		 return a * b;
+	}
+	
+	public double dividir(int a, int b) 
+	{
+		 return (double) a / b;
+	}
 }
